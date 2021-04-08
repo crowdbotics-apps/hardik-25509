63579796a92aeab8f04e20a7cb02cff6abcc1e9f
Crowdbotics <team@crowdbotics.com>
2021-04-08 05:05:48 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -9,7 +9,6 @@ export const installed_blueprints = [
 { name: 'CopyOfBlankScreen1222516', human_name: 'Copy of Blank Screen 1', access_route: 'CopyOfBlankScreen1222516'},
 { name: 'CopyOfBlankScreen1222515', human_name: 'Copy of Blank Screen 1', access_route: 'CopyOfBlankScreen1222515'},
 { name: 'CopyOfBlankScreen0222512', human_name: 'Copy of Blank Screen 0', access_route: 'CopyOfBlankScreen0222512'},
-{ name: 'CopyOfBlankScreen0222511', human_name: 'Copy of Blank Screen 0', access_route: 'CopyOfBlankScreen0222511'},
 { name: 'BlankScreen1222508', human_name: 'Blank Screen 1', access_route: 'BlankScreen1222508'},
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -14,7 +14,6 @@ import CopyOfBlankScreen1222517Navigator from '../features/CopyOfBlankScreen1222
 import CopyOfBlankScreen1222516Navigator from '../features/CopyOfBlankScreen1222516/navigator';
 import CopyOfBlankScreen1222515Navigator from '../features/CopyOfBlankScreen1222515/navigator';
 import CopyOfBlankScreen0222512Navigator from '../features/CopyOfBlankScreen0222512/navigator';
-import CopyOfBlankScreen0222511Navigator from '../features/CopyOfBlankScreen0222511/navigator';
 import BlankScreen1222508Navigator from '../features/BlankScreen1222508/navigator';
 
 /**
@@ -32,7 +31,6 @@ CopyOfBlankScreen1222517: { screen: CopyOfBlankScreen1222517Navigator },
 CopyOfBlankScreen1222516: { screen: CopyOfBlankScreen1222516Navigator },
 CopyOfBlankScreen1222515: { screen: CopyOfBlankScreen1222515Navigator },
 CopyOfBlankScreen0222512: { screen: CopyOfBlankScreen0222512Navigator },
-CopyOfBlankScreen0222511: { screen: CopyOfBlankScreen0222511Navigator },
 BlankScreen1222508: { screen: BlankScreen1222508Navigator },
 
   /** new navigators can be added here */
